d2b31838a10f8df92c0ac1e103afdc3f3b944846
unhappychoice <unhappychoice@gmail.com>
2015-11-15 06:41:40 +0900

    show stored token

--- app/src/main/kotlin/com/unhappychoice/norimaki/controllers/SettingFragment.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/controllers/SettingFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
+import android.widget.EditText
 import com.jakewharton.rxbinding.view.clicks
 import com.unhappychoice.norimaki.R
 import com.unhappychoice.norimaki.extension.toast
@@ -16,8 +16,10 @@ class SettingFragment: Fragment() {
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
     val view = inflater.inflate(R.layout.fragment_setting, container, false)
 
-    val token = view.findViewById(R.id.token) as TextView
+    val token = view.findViewById(R.id.token) as EditText
     val button = view.findViewById(R.id.save) as Button
+
+    token.setText(AccessToken(activity).value())
 
     button.clicks()
       .map { AccessToken(activity).store("${token.text}") }
