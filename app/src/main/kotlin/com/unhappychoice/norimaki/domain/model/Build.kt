be77cb9c8e0f6aadbc5c43fea31d165d15e61bed
Yuji Ueki <unhappychoice@gmail.com>
2017-05-07 23:11:36 +0900

    :bug: Show build num

--- app/src/main/kotlin/com/unhappychoice/norimaki/domain/model/Build.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/domain/model/Build.kt
@@ -4,7 +4,7 @@ import android.graphics.Color
 import com.github.unhappychoice.circleci.response.Build
 
 fun Build.repositoryString(): String = "$username / $reponame"
-fun Build.revisionString(): String = "$branch (${vcsRevision?.take(6)})"
+fun Build.revisionString(): String = "$branch #$buildNum (${vcsRevision?.take(6)})"
 
 fun Build.statusColor(): Int = when (status) {
   "success", "fixed", "no_tests" -> Color.rgb(66, 200, 138)
