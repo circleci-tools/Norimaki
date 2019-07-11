0e30f432b2a1d700a747110dc95456b8e1bf6708
Yuji Ueki <unhappychoice@gmail.com>
2019-07-11 23:07:50 +0900

    :bug: Fix pusher feature is not working

--- app/src/main/kotlin/com/unhappychoice/norimaki/domain/service/EventBusService.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/domain/service/EventBusService.kt
@@ -3,7 +3,6 @@ package com.unhappychoice.norimaki.domain.service
 import io.reactivex.subjects.PublishSubject
 
 class EventBusService {
-    val authenticated: PublishSubject<Pair<String, String>> = PublishSubject.create<Pair<String, String>>()
-    val unauthenticated: PublishSubject<Unit> = PublishSubject.create<Unit>()
-    val buildListUpdated: PublishSubject<Unit> = PublishSubject.create<Unit>()
+    val authenticated: PublishSubject<Pair<String, String>> = PublishSubject.create()
+    val unauthenticated: PublishSubject<Unit> = PublishSubject.create()
 }
