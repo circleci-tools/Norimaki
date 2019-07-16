6e0aceded7156c24c4a90b9a88072c88d0139443
Yuji Ueki <unhappychoice@gmail.com>
2019-07-17 05:27:50 +0900

    :+1: Show project list in navigation drawer

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/view/NavigationView.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/view/NavigationView.kt
@@ -4,23 +4,27 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.github.unhappychoice.circleci.CircleCIAPIClientV1
-import com.google.android.material.navigation.NavigationView as AndroidNavigationView
 import com.unhappychoice.norimaki.R
 import com.unhappychoice.norimaki.extension.subscribeNext
 import com.unhappychoice.norimaki.extension.subscribeOnIoObserveOnUI
-import com.unhappychoice.norimaki.extension.withLog
+import com.unhappychoice.norimaki.presentation.adapter.ProjectAdapter
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
 import kotlinx.android.synthetic.main.navigation_view.view.*
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.generic.instance
+import com.google.android.material.navigation.NavigationView as AndroidNavigationView
 
-class NavigationView(context: Context?, attr: AttributeSet?): AndroidNavigationView(context, attr), KodeinAware {
+class NavigationView(context: Context, attr: AttributeSet): AndroidNavigationView(context, attr), KodeinAware {
     override lateinit var kodein: Kodein
+
     private val client: CircleCIAPIClientV1 by instance()
+    private val adapter = ProjectAdapter(context)
     private val bag = CompositeDisposable()
 
     init {
@@ -31,8 +35,17 @@ class NavigationView(context: Context?, attr: AttributeSet?): AndroidNavigationV
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
 
+        projectsView.adapter = adapter
+        projectsView.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+
+        client.getProjects()
+            .subscribeOnIoObserveOnUI()
+            .subscribeNext {
+                adapter.projects.value = it
+                adapter.notifyDataSetChanged()
+            }.addTo(bag)
+
         client.getMe()
-            .withLog()
             .subscribeOnIoObserveOnUI()
             .subscribeNext {
                 Glide.with(context).load(it.avatarUrl).into(profileImageView)
