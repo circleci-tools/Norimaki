f905dfdd5c3325642345e10a946d4fdae7516674
Yuji Ueki <unhappychoice@gmail.com>
2020-11-01 16:53:20 +0900

    :green_heart: Fix build

--- app/src/test/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildScreenTest.kt
+++ app/src/test/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildScreenTest.kt
@@ -1,8 +1,8 @@
 package com.unhappychoice.norimaki.presentation.screen
 
 import com.github.unhappychoice.circleci.response.Build
-import com.nhaarman.mockito_kotlin.doReturn
-import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
 import com.unhappychoice.norimaki.R
 import com.unhappychoice.norimaki.domain.model.revisionString
 import com.winterbe.expekt.expect
--- app/src/test/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildStepScreenTest.kt
+++ app/src/test/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildStepScreenTest.kt
@@ -2,8 +2,8 @@ package com.unhappychoice.norimaki.presentation.screen
 
 import com.github.unhappychoice.circleci.response.Build
 import com.github.unhappychoice.circleci.response.BuildStep
-import com.nhaarman.mockito_kotlin.doReturn
-import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
 import com.unhappychoice.norimaki.R
 import com.winterbe.expekt.expect
 import io.polymorphicpanda.kspec.KSpec
